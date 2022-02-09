cf7f3032df96c403cab6b4672179f06fa4911116
Florian Boireau <florian23russie@hotmail.fr>
2022-02-09 11:55:05 +0100

    Pathfinding based, but still buged

--- Assets/Scripts/PlayerMoves.cs
+++ Assets/Scripts/PlayerMoves.cs
@@ -1,27 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerMoves : MonoBehaviour
 {
-    [SerializeField] float speed;
+    [SerializeField] private float speed;
+    private NavMeshAgent Agent;
 
     private Vector3 destination;
 
     // Start is called before the first frame update
     void Start()
     {
+        Agent = GetComponent<NavMeshAgent>();
         destination = new Vector3(0, 0, 0);   
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        transform.Translate(destination);
     }
 
     public void Move(Vector3 destination)
     {
-        this.destination = destination;
+        Agent.destination = destination;
     }
 }
