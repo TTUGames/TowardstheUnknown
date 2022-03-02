b706d6043679defb030c0c8fe1f87d02673c5d4d
Florian Boireau <florian23russie@hotmail.fr>
2022-03-02 13:24:44 +0100

    Fixed the timer bug
    It wasn't launching sometimes

--- Assets/Scripts/Gameplay/TimerCountdown.cs
+++ Assets/Scripts/Gameplay/TimerCountdown.cs
@@ -2,26 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Turn timer
+/// </summary>
 public class TimerCountdown : MonoBehaviour
 {
     public float timeToEndTurn = 30f;
 
     public float timeRemaining;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        timeRemaining = timeToEndTurn;
-    }
-
     
+    /// <summary>
+    /// Launch the timer
+    /// </summary>
     public void LaunchTimer()
     {
         StartCoroutine(Countdown());
     }
 
+    /// <summary>
+    /// Decrement the <c>timeRemaining each second</c>
+    /// </summary>
+    /// <returns>Nothing</returns>
     private IEnumerator Countdown()
     {
+        Debug.Log(timeRemaining);
+        timeRemaining = timeToEndTurn;
         while (timeRemaining > 0)
         {
             timeRemaining--;
@@ -33,6 +38,9 @@ public class TimerCountdown : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Reset the timer to the default value <c>timeToEndTurn</c>
+    /// </summary>
     public void resetTimer()
     {
         timeRemaining = timeToEndTurn;
