dd7049b3ae0d5b4c141dc236d69d681527099465
Florian Boireau <florian23russie@hotmail.fr>
2022-03-03 16:01:27 +0100

    Added a way to store the precedent turn remaining time | Not Tested

--- Assets/Scripts/Gameplay/Timer.cs
+++ Assets/Scripts/Gameplay/Timer.cs
@@ -9,39 +9,61 @@ public class Timer : MonoBehaviour
 {
     public float timeToEndTurn = 30f;
 
-    public float timeRemaining;
+    public  float timeRemaining; //TODO after Timer is done, put in private
+    private float TimeRemainingInPrecedentTurn;
+
+    private bool stopTimer = false;
     
     /// <summary>
     /// Launch the timer
     /// </summary>
     public void LaunchTimer()
     {
+        stopTimer = false;
         StartCoroutine(Countdown());
     }
 
+    public void StopTimer()
+    {
+        stopTimer = true;
+    }
+
     /// <summary>
-    /// Decrement the <c>timeRemaining each second</c>
+    /// Decrement the <c>timeRemaining each second</c> <br/>
+    /// Uses the current turn time remaining then if it reach 0, uses the time remaining from precedent turn
     /// </summary>
     /// <returns>Nothing</returns>
     private IEnumerator Countdown()
     {
-        Debug.Log(timeRemaining);
         timeRemaining = timeToEndTurn;
-        while (timeRemaining > 0)
+        while (timeRemaining > 0 && TimeRemainingInPrecedentTurn > 0)
         {
-            timeRemaining--;
-            yield return new WaitForSeconds(1);
+            if (!stopTimer)
+            {
+                timeRemaining--;
+
+                if(timeRemaining == 0)
+                {
+                    timeRemaining = TimeRemainingInPrecedentTurn;
+                    TimeRemainingInPrecedentTurn = 0;
+                }
+
+                yield return new WaitForSeconds(1);
+            }
+            else
+            {
+                TimeRemainingInPrecedentTurn = timeRemaining;
+                yield break;
+            }
         }
-        if(timeRemaining == 0)
-        {
-            //FINISH TURN
-        }
+        TimeRemainingInPrecedentTurn = 0;
+        //FINISH TURN
     }
 
     /// <summary>
     /// Reset the timer to the default value <c>timeToEndTurn</c>
     /// </summary>
-    public void resetTimer()
+    public void ResetTimer()
     {
         timeRemaining = timeToEndTurn;
     }
