7ceb2c69cf680a360c1a009e6339f7dfad296a81
Florian Boireau <florian23russie@hotmail.fr>
2022-03-03 11:53:05 +0100

    Added comment and move some file to make the script file a better place

--- Assets/Scripts/Player-NPC/HealthSystem.cs
+++ Assets/Scripts/Player-NPC/HealthSystem.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
+public class HealthSystem : MonoBehaviour
 {
     public int maxHealth;
     public int currentHealth;
 
+    /// <summary>
+    /// Increase the maximum health
+    /// </summary>
+    /// <param name="healthAdded">Health to add</param>
     public void IncreaseMaxHealth(int healthAdded)
     {
         maxHealth += healthAdded;
     }
 
+    /// <summary>
+    /// Decrease the maximum health
+    /// </summary>
+    /// <param name="healthRemoved">Health to remove</param>
     public void DecreaseMaxHealth(int healthRemoved)
     {
         maxHealth -= healthRemoved;
     }
 
+    /// <summary>
+    /// Increase the health
+    /// </summary>
+    /// <param name="healthAdded">Health to add</param>
     public void GainHealth(int healthAdded)
     {
         currentHealth += healthAdded;
@@ -25,6 +37,10 @@ public class PlayerHealth : MonoBehaviour
             currentHealth = maxHealth;
     }
 
+    /// <summary>
+    /// Decrease the health
+    /// </summary>
+    /// <param name="healthRemoved">Health to remove</param>
     public void LoseHealth(int healthRemoved)
     {
         currentHealth -= healthRemoved;
