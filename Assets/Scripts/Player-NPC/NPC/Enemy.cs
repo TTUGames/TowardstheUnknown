5c3140ae9f1a3af2c6927ad986d6909eabf83d06
Florian Boireau <florian23russie@hotmail.fr>
2022-03-04 23:47:13 +0100

    Turn system works, need to do the reset of movement, actions. Add deploy

--- Assets/Scripts/Player-NPC/NPC/Enemy.cs
+++ Assets/Scripts/Player-NPC/NPC/Enemy.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    
+    public void LaunchTurn()
+    {
+        Debug.Log("Entity : " + transform.name + " | Start his turn");
+    }
+
+    public void StopTurn()
+    {
+        Debug.Log("Entity : " + transform.name + " | End his turn");
+    }
 }
