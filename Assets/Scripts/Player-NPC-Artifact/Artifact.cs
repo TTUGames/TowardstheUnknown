cb05c3e88ae3109cc0e6b3d8845e1648e8f74b68
Florian Boireau <florian23russie@hotmail.fr>
2022-03-21 12:13:08 +0100

    Artifact update -_-'

--- Assets/Scripts/Player-NPC-Artifact/Artifact.cs
+++ Assets/Scripts/Player-NPC-Artifact/Artifact.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class Artifact : MonoBehaviour
 {
+    [SerializeField] private int cost;
+
+    [SerializeField] private int distanceMin;
+    [SerializeField] private int distanceMax;
+
+    [SerializeField] private int areaOfEffectMin;
+    [SerializeField] private int areaOfEffectMax;
+
+    [SerializeField] private int damageMin;
+    [SerializeField] private int damageMax;
+
+    [SerializeField] private int MaximumUsePerTurn;
+    [SerializeField] private int cooldown;
+    [SerializeField] private int lootRate;
+
+    [SerializeField] private int sizeX;
+    [SerializeField] private int sizeY;
+
+
     // Start is called before the first frame update
     void Start()
     {
