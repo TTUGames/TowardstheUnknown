f415e68acde96902f2df42aa8f4979f223e1415a
Florian Boireau <florian23russie@hotmail.fr>
2022-10-09 20:30:39 +0200

    Skillbar in UI done (finally found the idea)

--- Assets/Scripts/UI/UISkillsBar.cs
+++ Assets/Scripts/UI/UISkillsBar.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class UISkillsBar : MonoBehaviour
 {
+    [SerializeField] private float skillSize = 0.025f;
     private TempUITestStats tempUITestStats;
 
     private void Awake()
@@ -24,6 +25,12 @@ public class UISkillsBar : MonoBehaviour
             GameObject.Destroy(child.gameObject);
         }
         
+        GetComponent<RectTransform>().anchorMin = new Vector2(0.5f - skillSize * tempUITestStats.lObjects.Count, GetComponent<RectTransform>().anchorMin.y);
+        GetComponent<RectTransform>().anchorMax = new Vector2(0.5f + skillSize * tempUITestStats.lObjects.Count, GetComponent<RectTransform>().anchorMax.y);
+
+
+        float previousAnchorMaxPoint = 0f;
+        float anchorMaxXSize = (1f - previousAnchorMaxPoint) / tempUITestStats.lObjects.Count;
         for (int i = 0; i < tempUITestStats.lObjects.Count; i++)
         {
             GameObject skill = new GameObject();
@@ -33,12 +40,13 @@ public class UISkillsBar : MonoBehaviour
             
             skill.AddComponent<Image>();
             if(i%2 == 0)
-                skill.GetComponent<Image>().color = Color.red;
+                skill.GetComponent<Image>().color = Color.grey;
             skill.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
             skill.GetComponent<RectTransform>().sizeDelta = new Vector2(10, 10);
             skill.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.5f, 0.5f);
-            skill.GetComponent<RectTransform>().anchorMin = new Vector2((float)i / 10, 0);
-            skill.GetComponent<RectTransform>().anchorMax = new Vector2((float)i / 10 + 0.10f, 1f);
+            skill.GetComponent<RectTransform>().anchorMin = new Vector2(previousAnchorMaxPoint, 0);
+            skill.GetComponent<RectTransform>().anchorMax = new Vector2(previousAnchorMaxPoint + anchorMaxXSize, 1f);
+            previousAnchorMaxPoint = skill.GetComponent<RectTransform>().anchorMax.x;
             skill.GetComponent<RectTransform>().offsetMin = new Vector2(0, 0);
             skill.GetComponent<RectTransform>().offsetMax = new Vector2(0, 0);
             print(skill.GetComponent<RectTransform>().offsetMin);
