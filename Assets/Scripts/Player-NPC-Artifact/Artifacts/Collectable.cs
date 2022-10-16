0bbcdb81bc13aa7ab3d52e09fb2b8eaaef6f70e5
Florian Boireau <florian23russie@hotmail.fr>
2022-10-16 23:33:11 +0200

    Created Collectable Script
    Will be use to take the item from the map to the inventory

---
+++ Assets/Scripts/Player-NPC-Artifact/Artifacts/Collectable.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    // enable pickingUp items (K)
+    public Vector3 posToGo;
+    public TetrisItem itemTetris;
+
+    private void OnTriggerStay3D(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                bool wasPickedUpTestris = false;
+                wasPickedUpTestris = TetrisSlot.instanceSlot.addInFirstSpace(itemTetris); //add to the bag matrix.
+                if (wasPickedUpTestris) // took
+                {
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+    }
+
+}
