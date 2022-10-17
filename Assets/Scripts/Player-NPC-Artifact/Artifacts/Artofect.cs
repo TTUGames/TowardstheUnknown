71760e75f6c0bcd0166bb09a46d7ed4cf7d61322
Florian Boireau <florian23russie@hotmail.fr>
2022-10-17 02:31:31 +0200

    Created Artofect, a bullshit thing just to know if Scriptable are better

---
+++ Assets/Scripts/Player-NPC-Artifact/Artifacts/Artofect.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Artofect", menuName = "Inventory/Tetris/Item")]
+public class Artofect : ScriptableObject
+{
+    [SerializeField] private GameObject prefab;
+
+    [SerializeField] protected string title;
+    [SerializeField] protected string description;
+    [SerializeField] protected Sprite icon;
+
+    [SerializeField] protected int cost;
+
+    [SerializeField] protected int distanceMin;
+    [SerializeField] protected int distanceMax;
+
+    [SerializeField] protected int areaOfEffectMin;
+    [SerializeField] protected int areaOfEffectMax;
+
+    [SerializeField] protected int damageMin;
+    [SerializeField] protected int damageMax;
+
+    [SerializeField] protected int maximumUsePerTurn;
+    [SerializeField] protected int cooldown;
+    [SerializeField] protected float lootRate;
+
+    [SerializeField] protected int sizeX;
+    [SerializeField] protected int sizeY;
+
+
+
+    /***********************/
+    /*                     */
+    /*  GETTERS | SETTERS  */
+    /*                     */
+    /***********************/
+    public GameObject Prefab { get => prefab; set => prefab = value; }
+    public string Title { get => title; set => title = value; }
+    public string Description { get => description; set => description = value; }
+    public Sprite Icon { get => icon; set => icon = value; }
+    public int Cost { get => cost; set => cost = value; }
+    public int DistanceMin { get => distanceMin; set => distanceMin = value; }
+    public int DistanceMax { get => distanceMax; set => distanceMax = value; }
+    public int AreaOfEffectMin { get => areaOfEffectMin; set => areaOfEffectMin = value; }
+    public int AreaOfEffectMax { get => areaOfEffectMax; set => areaOfEffectMax = value; }
+    public int DamageMin { get => damageMin; set => damageMin = value; }
+    public int DamageMax { get => damageMax; set => damageMax = value; }
+    public int MaximumUsePerTurn { get => maximumUsePerTurn; set => maximumUsePerTurn = value; }
+    public int Cooldown { get => cooldown; set => cooldown = value; }
+    public float LootRate { get => lootRate; set => lootRate = value; }
+    public int SizeX { get => sizeX; set => sizeX = value; }
+    public int SizeY { get => sizeY; set => sizeY = value; }
+
+}
