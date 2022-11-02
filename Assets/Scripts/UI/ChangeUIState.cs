65c9a667f2b9fef755a264297a06d39f5e6cc223
Florian Boireau <florian23russie@hotmail.fr>
2022-11-02 18:22:12 +0100

    Inventory UI (PREFAB) can now be seen or unseen with I

---
+++ Assets/Scripts/UI/ChangeUIState.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeUIState : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            print("la");
+            ChangeStateInventory();
+        }
+    }
+
+    private void ChangeStateInventory()
+    {
+        foreach (Transform child in transform.GetChild(0))
+        {
+            print(child.name + " " + child.gameObject.activeSelf);
+            if (child.name == "TetrisInventory" && child.gameObject.activeSelf == false)
+            {
+                child.gameObject.SetActive(true);
+                foreach (Transform child2 in transform.GetChild(0))
+                    if (child2.name == "Button" || child2.name == "Health" || child2.name == "Skills" || child2.name == "Timer" || child2.name == "Energy")
+                        child2.gameObject.SetActive(false);
+            }
+            else if(child.name == "TetrisInventory" && child.gameObject.activeSelf == true)
+            {
+                child.gameObject.SetActive(false);
+                foreach (Transform child2 in transform.GetChild(0))
+                    if (child2.name == "Button" || child2.name == "Health" || child2.name == "Skills" || child2.name == "Timer" || child2.name == "Energy")
+                        child2.gameObject.SetActive(true);
+            }
+        }
+    }
+}
