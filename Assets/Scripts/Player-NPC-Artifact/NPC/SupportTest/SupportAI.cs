a762c4542d7337a22e498220a3dcb3bfe34ab12a
raphael lesbros <raphael.lesbros@gmail.com>
2022-11-14 09:20:28 +0100

    cleaned up enemy patterns

--- Assets/Scripts/Player-NPC-Artifact/NPC/SupportTest/SupportAI.cs
+++ Assets/Scripts/Player-NPC-Artifact/NPC/SupportTest/SupportAI.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class SupportAI : EnemyAI {
 	protected override void SetAttackPatterns() {
 		attack.AddPattern(new SupportBuffPattern());
