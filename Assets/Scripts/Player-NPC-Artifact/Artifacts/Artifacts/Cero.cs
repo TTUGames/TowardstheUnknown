cb17d1aafb55aa7a2036f0834e92830c707e327a
Florian Boireau <florian23russie@hotmail.fr>
2022-11-17 19:44:07 +0100

    Created Cero script (nothing new)

---
+++ Assets/Scripts/Player-NPC-Artifact/Artifacts/Artifacts/Cero.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cero : SingleTargetArtifact
+{
+	public Cero() {
+		Prefab = (GameObject)Resources.Load("VFX/CeroOscuras/Cero", typeof(GameObject));
+		AnimStateName = "Cero";
+
+		icon = (Sprite)Resources.Load("Sprites/" + GetType().Name, typeof(Sprite));
+        
+        cost = 2;
+
+		range = new CircleAttackTS(2, 5);
+
+		maximumUsePerTurn = 2;
+		cooldown = 0;
+
+		size = new Vector2(2, 3);
+		lootRate = 0.01f;
+
+		targets.Add("Enemy");
+	}
+
+	public override void ApplyEffects(PlayerStats source, EntityStats target) {
+		ActionManager.AddToBottom(new DamageAction(source, target, 45, 55));
+	}
+}
