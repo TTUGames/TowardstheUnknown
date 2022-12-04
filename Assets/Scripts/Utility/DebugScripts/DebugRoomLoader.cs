8456edbb4cb5dbe52e74b0bc4c7416da6782b786
Florian Boireau <florian23russie@hotmail.fr>
2022-12-04 23:30:34 +0100

    Tiles now get a random mesh (from a list) when the room is generated

--- Assets/Scripts/Utility/DebugScripts/DebugRoomLoader.cs
+++ Assets/Scripts/Utility/DebugScripts/DebugRoomLoader.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DebugRoomLoader : MonoBehaviour
 {
     public Room prefab;
+	[SerializeField] private List<GameObject> lTilePossible;
 	public int layoutIndex;
 
 	private void Start() {
@@ -14,7 +16,17 @@ public class DebugRoomLoader : MonoBehaviour
 	private IEnumerator LoadRoom() {
 		RoomInfo roomInfo = new RoomInfo(prefab, layoutIndex);
 		Room currentRoom = roomInfo.LoadRoom(false, false, false, false);
-		yield return currentRoom.GetComponent<PlayerDeploy>().DeployPlayer(FindObjectOfType<PlayerTurn>().transform, Direction.NORTH);
+        ReloadTilesWithRandomPrefab();
+        yield return currentRoom.GetComponent<PlayerDeploy>().DeployPlayer(FindObjectOfType<PlayerTurn>().transform, Direction.NORTH);
 		FindObjectOfType<TurnSystem>().CheckForCombatStart();
 	}
+
+    private void ReloadTilesWithRandomPrefab()
+    {
+        GameObject.FindGameObjectsWithTag("Tile").ToList().ForEach(tile =>
+        {
+            int randomIndex = Random.Range(0, lTilePossible.Count);
+            tile.GetComponent<MeshFilter>().sharedMesh = lTilePossible[randomIndex].GetComponent<MeshFilter>().sharedMesh;
+        });
+    }
 }
