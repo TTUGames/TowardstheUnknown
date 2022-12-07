1f031f2f8b2b70ca4dd9f809455591e904bd3b99
Laconiq <laconiqofficial@gmail.com>
2022-12-07 11:09:37 +0100

    New AI : Kameiko & GreatKameiko

---
+++ Assets/Scripts/Player-NPC-Artifact/NPC/Kameiko/KameikoAI.cs
@@ -0,0 +1,11 @@
+public class KameikoAI : EnemyAI
+{
+
+	protected override void SetAttackPatterns() {
+		attack.AddPattern(new KameikoSlashAttackPattern());
+	}
+
+	protected override void SetTargetting() {
+		targetting = new PlayerTargetting(2);
+	}
+}
