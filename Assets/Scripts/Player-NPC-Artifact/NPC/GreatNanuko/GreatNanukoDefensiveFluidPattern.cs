baf47874368b15cba6bfe6d8dbe8ccdf0c900699
Laconiq <laconiqofficial@gmail.com>
2022-12-07 10:20:09 +0100

    New AI : GreatNanuko

---
+++ Assets/Scripts/Player-NPC-Artifact/NPC/GreatNanuko/GreatNanukoDefensiveFluidPattern.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GreatNanukoDefensiveFluidPattern : EnemyPattern {
+	public override void Init() {
+		patternDuration = 2f;
+		range = new LineAttackTS(0, 0);
+		//vfxPrefab = Resources.Load<GameObject>("VFX/NanukoPaw/NanukoStrike");
+		targetType = EntityType.PLAYER;
+	}
+
+	public override void Use(EntityStats source, EntityStats target) {
+		ActionManager.AddToBottom(new ApplyStatusAction(target, new DefenseUpStatus(2)));
+	}
+
+	public override void PlayAnimation(Tile sourceTile, Tile targetTile, GameObject source) {
+		float rotation = -Vector3.SignedAngle(targetTile.transform.position - sourceTile.transform.position, Vector3.forward, Vector3.up);
+		source.transform.rotation = Quaternion.Euler(0, rotation, 0);
+
+
+		Animator animator = source.GetComponent<Animator>();
+		if (animator != null && animStateName != "") animator.Play(animStateName);
+
+		GameObject vfx = null;
+		if (vfxPrefab != null) {
+			Vector3 VFXposition = targetTile.transform.position;
+			VFXposition.y += 1.5f;
+
+			vfx = GameObject.Instantiate(vfxPrefab, VFXposition, Quaternion.Euler(0, 0 + rotation, 0));
+		}
+
+		ActionManager.AddToBottom(new WaitForAttackEndAction(patternDuration, source, vfx));
+	}
+}
