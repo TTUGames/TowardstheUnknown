6e6bad3ec4bf5f7c9c3347f1772945b302dd2dd3
raphael lesbros <raphael.lesbros@gmail.com>
2022-12-10 09:34:47 +0100

    Fix move towards action sometimes crashing

--- Assets/Scripts/Player-NPC-Artifact/Actions/MoveTowardsAction.cs
+++ Assets/Scripts/Player-NPC-Artifact/Actions/MoveTowardsAction.cs
@@ -19,8 +19,12 @@ public class MoveTowardsAction : Action
 		TacticsMove targetMove = target.GetComponent<TacticsMove>();
 
 		Vector3 direction = targetMove.CurrentTile.transform.position - sourceMove.CurrentTile.transform.position;
-		if (!(direction.x == 0 ^ direction.z == 0)) throw new System.Exception("Target and Source are not valid for MoveTowardsAction");
-		direction = direction.normalized;
+
+		//Normalize the vector, must be done manually because the float substraction doesn't always result in 0
+		if (Mathf.Abs(direction.x) <= 0.01 && Mathf.Abs(direction.z) >= 0.99) direction = Vector3.forward;
+		else if (Mathf.Abs(direction.z) <= 0.01 && Mathf.Abs(direction.x) >= 0.99) direction = Vector3.right;
+		else throw new System.Exception("Target and Source are not valid for MoveTowardsAction");
+
 		if (distance < 0) direction = -direction;
 
 		List<Tile> path = new List<Tile>();
@@ -34,7 +38,7 @@ public class MoveTowardsAction : Action
 			path.Add(targetTile);
 		}
 		path.Reverse();
-
+		Debug.Log(path.Count);
 		sourceMove.MoveToTile(targetTile, new Stack<Tile>(path), false);
 
 		isDone = true;
