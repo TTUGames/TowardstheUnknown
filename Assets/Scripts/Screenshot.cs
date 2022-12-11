a47998bfb20d565678c7c306b342b412f65602e2
Laconiq <laconiqofficial@gmail.com>
2022-12-11 19:42:39 +0100

    Screenshot path in the inspector

--- Assets/Scripts/Screenshot.cs
+++ Assets/Scripts/Screenshot.cs
@@ -1,16 +1,20 @@
-using UnityEngine; 
- 
-public class Screenshot : MonoBehaviour 
-{ 
-    // Prend une capture d'écran et l'enregistre dans un fichier dans le répertoire "~/Pictures". 
-    void Update() 
-    { 
-        if (Input.GetKeyDown(KeyCode.F12)) 
-        { 
-            string timestamp = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"); 
-            string filename = "screenshot_" + timestamp + ".png"; 
-            string path = System.IO.Path.Combine(Application.persistentDataPath, "/Pictures", filename); 
-            ScreenCapture.CaptureScreenshot(path); 
-        } 
-    } 
-} 
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    // Variable publique pour stocker le chemin d'accès au répertoire cible.
+    // Cette variable apparaîtra dans l'inspector et pourra être modifiée par l'utilisateur.
+    public string screenshotPath = "/Pictures";
+
+    // Prend une capture d'écran et l'enregistre dans un fichier dans le répertoire spécifié par screenshotPath.
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            string timestamp = System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            string filename = "screenshot_" + timestamp + ".png";
+            string path = System.IO.Path.Combine(Application.persistentDataPath, screenshotPath, filename);
+            ScreenCapture.CaptureScreenshot(path);
+        }
+    }
+}
