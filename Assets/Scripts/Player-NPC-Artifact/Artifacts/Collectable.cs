775899143a3aa58d95e7a76e3459888ebf731cd7
raphael lesbros <raphael.lesbros@gmail.com>
2022-12-15 10:08:59 +0100

    Added comments

--- Assets/Scripts/Player-NPC-Artifact/Artifacts/Collectable.cs
+++ Assets/Scripts/Player-NPC-Artifact/Artifacts/Collectable.cs
@@ -8,12 +8,20 @@ public class Collectable : MonoBehaviour
     public Vector3 posToGo;
     [SerializeField] private string artifactName;
     
+    /// <summary>
+    /// Tries to pickup the item when the player enters the collision
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
             TryPickUp();
     }
     
+    /// <summary>
+    /// Tries to pickup the item when E is pressed while colliding
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -25,6 +33,9 @@ public class Collectable : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Tries to pickup this item, and destroys it if successful
+    /// </summary>
     private void TryPickUp() {
         bool wasPickedUp = false;
         Artifact artifact = (Artifact)Activator.CreateInstance(Type.GetType(artifactName));
