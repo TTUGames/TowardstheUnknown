b8291f7b0f5a7dfd263c286583b03c9176f99691
raphael lesbros <raphael.lesbros@gmail.com>
2022-12-17 10:15:49 +0100

    Added ApplyEffectOnCast to AOEArtifacts

--- Assets/Scripts/Player-NPC-Artifact/Artifacts/AbstractArtifacts/AoeArtifact.cs
+++ Assets/Scripts/Player-NPC-Artifact/Artifacts/AbstractArtifacts/AoeArtifact.cs
@@ -12,6 +12,7 @@ public abstract class AoeArtifact : Artifact {
 	public override void Launch(PlayerAttack source, Tile tile) {
         ApplyCosts(source.Stats);
 
+        ApplyEffectOnCast(source.Stats);
         foreach (Tile target in GetTargets(tile)) {
             EntityStats entity = target.GetEntity();
             if (entity == null || !targets.Contains(entity.tag))
@@ -21,6 +22,14 @@ public abstract class AoeArtifact : Artifact {
 
         PlayAnimation(source.CurrentTile, tile, source);
     }
+
+    /// <summary>
+    /// Effects that must be applied exactly once, not taking the number of targets into account
+    /// </summary>
+    /// <param name="source"></param>
+    protected virtual void ApplyEffectOnCast(EntityStats source) {
+
+	}
 
 	public override List<Tile> GetTargets(Tile targetedTile) {
         if (targetedTile == null || targetedTile.Selection != Tile.SelectionType.ATTACK) return new List<Tile>();
