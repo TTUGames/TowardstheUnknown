ed67099195ee7ad3c91964124055cf1fceffbdb3
raphael lesbros <raphael.lesbros@gmail.com>
2023-01-09 19:39:09 +0100

    Added Steamworks

--- Assets/Scripts/Localization/Localization.cs
+++ Assets/Scripts/Localization/Localization.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Steamworks;
 
 public static class Localization {
     private static readonly string localizationPath = "Localization/";
