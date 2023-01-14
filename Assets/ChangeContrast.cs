6dd7e088699d7bccf73e0d018eddee6da800ecd2
Laconiq <laconiqofficial@gmail.com>
2023-01-14 15:38:41 +0100

    Save contrast

--- Assets/ChangeContrast.cs
+++ Assets/ChangeContrast.cs
@@ -8,14 +8,29 @@ public class ChangeContrast : MonoBehaviour
     public Slider slider;
     public Volume volume;
 
-    void Start()
+    void Awake()
     {
-        slider.onValueChanged.AddListener(UpdatePostExposure);        
+        float savedContrastValue = PlayerPrefs.GetFloat("contrastValue", .5f);
+
+        float lerpedValue = Mathf.Lerp(-100, 100, savedContrastValue);
+        
+        ClampedFloatParameter contrastParameter = new ClampedFloatParameter(lerpedValue, -100, 100, true);
+        volume.profile.components[0].parameters[1].SetValue(contrastParameter); 
     }
 
-    void UpdatePostExposure(float value)
+    void Start()
     {
-        ClampedFloatParameter test = new ClampedFloatParameter(Mathf.Lerp(-100, 100, value), -100, 100, true);
-        volume.profile.components[0].parameters[1].SetValue(test);
+        slider.onValueChanged.AddListener(UpdateContrast);
+        slider.value = PlayerPrefs.GetFloat("contrastValue", 0);
+    }
+
+    void UpdateContrast(float value)
+    {
+        PlayerPrefs.SetFloat("contrastValue", value);
+        
+        float lerpedValue = Mathf.Lerp(-100, 100, value);
+        
+        ClampedFloatParameter contrastParameter = new ClampedFloatParameter(lerpedValue, -100, 100, true);
+        volume.profile.components[0].parameters[1].SetValue(contrastParameter);
     }
 }
