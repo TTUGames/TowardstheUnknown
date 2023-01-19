e742f886aeaec85f078eb5a18738db556f96d7e8
Laconiq <laconiqofficial@gmail.com>
2023-01-19 20:18:00 +0100

    Kameiko & Nanuko have now attacking animations

--- Assets/Scripts/Player-NPC-Artifact/NPC/Kameiko/KameikoSlashAttackPattern.cs
+++ Assets/Scripts/Player-NPC-Artifact/NPC/Kameiko/KameikoSlashAttackPattern.cs
@@ -6,6 +6,7 @@ public class KameikoSlashAttackPattern : EnemyPattern {
 		range = new LineAttackTS(1, 1);
 		//vfxInfos.Add(new VFXInfo("VFX/BlackHole/BlackHole", VFXInfo.Target.TARGETTILE, 0, Vector3.up * 1.5f));
 		targetType = EntityType.PLAYER;
+		animStateName = "KameikoSlashAttackPattern";
 	}
 
 	public override void Use(EntityStats source, EntityStats target) {
