d5d9fa1fb5f9657d9c3c6ce33fcd65797cf1838c
Laconiq <laconiqofficial@gmail.com>
2023-01-21 20:13:16 +0100

    Switch down the light in pause mode

--- Assets/Scripts/UI/UIPause.cs
+++ Assets/Scripts/UI/UIPause.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class UIPause : MonoBehaviour
 {
     public GameObject pause;
+    public GameObject backgroundPause;
     public GameObject PauseMain;
     public GameObject PauseOptions;
     public bool isPaused = false;
     public Animator animator;
+    public Animator backgroundAnimator;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -22,11 +24,15 @@ public class UIPause : MonoBehaviour
             {
                 isPaused = false;
                 animator.Play("PauseMenuAnimationOff");
+                //backgroundAnimator.Play("Off");
+                backgroundPause.SetActive(false);
             }
             else
             {
+                backgroundPause.SetActive(true);
                 isPaused = true;
                 animator.Play("PauseMenuAnimationOn");
+                backgroundAnimator.Play("On");
             }
         }
     }
