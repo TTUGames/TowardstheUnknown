2ca7f52efb025e658775d672a129704bc147a622
Laconiq <laconiqofficial@gmail.com>
2023-01-26 21:42:13 +0100

    Golem : Add dying and attacking animations p2

--- Assets/Scripts/Player-NPC-Artifact/NPC/Golem/GolemRockFallPattern.cs
+++ Assets/Scripts/Player-NPC-Artifact/NPC/Golem/GolemRockFallPattern.cs
@@ -4,8 +4,9 @@ public class GolemRockFallPattern : EnemyPattern {
 	public override void Init() {
 		patternDuration = 2f;
 		range = new CircleAttackTS(2, 5);
-		vfxInfos.Add(new VFXInfo("VFX/NanukoPaw/NanukoStrike", VFXInfo.Target.TARGETTILE, 0, Vector3.up * 1.5f));
+		//vfxInfos.Add(new VFXInfo("VFX/NanukoPaw/NanukoStrike", VFXInfo.Target.TARGETTILE, 0, Vector3.up * 1.5f));
 		targetType = EntityType.PLAYER;
+		animStateName = "GolemRockFallPattern";
 	}
 
 	public override void Use(EntityStats source, EntityStats target) {
--- Assets/Scripts/Player-NPC-Artifact/NPC/Golem/GolemShockWavePattern.cs
+++ Assets/Scripts/Player-NPC-Artifact/NPC/Golem/GolemShockWavePattern.cs
@@ -4,8 +4,9 @@ public class GolemShockWavePattern : EnemyPattern {
 	public override void Init() {
 		patternDuration = 2f;
 		range = new CircleAttackTS(1, 1);
-		vfxInfos.Add(new VFXInfo("VFX/BlackHole/BlackHole", VFXInfo.Target.TARGETTILE, 0, Vector3.up * 1.5f));
+		//vfxInfos.Add(new VFXInfo("VFX/BlackHole/BlackHole", VFXInfo.Target.TARGETTILE, 0, Vector3.up * 1.5f));
 		targetType = EntityType.PLAYER;
+		animStateName = "GolemShockWavePattern";
 	}
 
 	public override void Use(EntityStats source, EntityStats target) {
