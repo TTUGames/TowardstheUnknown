99be1bf6d3659f2a1a6d7660460f7b173433f9ce
Laconiq <laconiqofficial@gmail.com>
2023-01-28 11:58:33 +0100

    Drareg : Add animations

--- Assets/Scripts/Player-NPC-Artifact/NPC/Drareg/DraregPrecisionShootPattern.cs
+++ Assets/Scripts/Player-NPC-Artifact/NPC/Drareg/DraregPrecisionShootPattern.cs
@@ -6,6 +6,7 @@ public class DraregPrecisionShootPattern : EnemyPattern {
 		range = new CircleAttackTS(3, 5);
 		vfxInfos.Add(new VFXInfo("VFX/NanukoPaw/NanukoStrike", VFXInfo.Target.TARGETTILE, 0, Vector3.up * 1.5f));
 		targetType = EntityType.PLAYER;
+		animStateName = "PrecisionShoot";
 	}
 
 	public override void Use(EntityStats source, EntityStats target) {
