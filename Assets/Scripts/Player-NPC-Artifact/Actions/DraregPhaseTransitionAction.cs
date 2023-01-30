c143cb9d86d8cccd3ebd66b3ab39e88e7554ad1a
raphael lesbros <raphael.lesbros@gmail.com>
2023-01-30 08:42:39 +0100

    Added time at max progress for drareg vfx

--- Assets/Scripts/Player-NPC-Artifact/Actions/DraregPhaseTransitionAction.cs
+++ Assets/Scripts/Player-NPC-Artifact/Actions/DraregPhaseTransitionAction.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class DraregPhaseTransitionAction : Action {
 	private GameObject vfx;
 
-	private float duration = 4f;
+	private float transitionDuration = 4f; //Duration of vfx progress going from minVFXScale to 1
+	private float staticDuration = 2f; //Duration of vfx progress staying at 1
 	private Color startColor = Color.blue;
 	private Color endColor = Color.red;
 	private float minVFXProgress = -1f;
@@ -19,19 +20,32 @@ public class DraregPhaseTransitionAction : Action {
 	}
 
 	private IEnumerator VFXUpdate(DraregAI drareg) {
-		float endTime = Time.time + duration;
+		float startTime = Time.time;
+		float totalDuration = 2 * transitionDuration + staticDuration;
+		float endTime = startTime + totalDuration;
+
 		Renderer vfxRenderer = vfx.GetComponent<Renderer>();
 		bool switchedModel = false;
 
 		while (Time.time < endTime) {
-			float progress = 1 - (endTime - Time.time) / duration; //progress goes from 0 at the beginning to 1 at the end of the duration
-			if (!switchedModel && progress > 0.5f) {
-				drareg.SwitchModel();
-				switchedModel = true;
+			float currentTime = Time.time - startTime;
+			if (currentTime <= transitionDuration) { //Increase phase
+				vfxRenderer.sharedMaterial.SetFloat("AppearProgress__1", (currentTime / transitionDuration) * (1 - minVFXProgress) + minVFXProgress);
 			}
-			vfxRenderer.sharedMaterial.SetFloat("AppearProgress__1", Mathf.Abs(progress - 0.5f) * (2 * minVFXProgress - 2) + 1); //Transforms progress into a float that goes from minVFXProgress to 1 to minVFXProgress
+			else if (currentTime <= transitionDuration + staticDuration) { //Static phase
+				vfxRenderer.sharedMaterial.SetFloat("AppearProgress__1", 1);
+				if (!switchedModel) {
+					drareg.SwitchModel();
+					switchedModel = true;
+				}
+			}
+			else { //Decrease phase
+				vfxRenderer.sharedMaterial.SetFloat("AppearProgress__1", ((totalDuration - currentTime) / transitionDuration) * (1 - minVFXProgress) + minVFXProgress);
+			}
+
+			float progress = currentTime / totalDuration; //progress goes from 0 at the beginning to 1 at the end of the duration
 			vfxRenderer.sharedMaterial.SetColor("RampColorTint_", new Color(startColor.r + (endColor.r - startColor.r) * progress, startColor.g + (endColor.g - startColor.g) * progress, startColor.b + (endColor.b - startColor.b) * progress)); //Linearly switches color from startColor to endColor
-			
+
 			yield return null;
 		}
 
@@ -40,6 +54,6 @@ public class DraregPhaseTransitionAction : Action {
 	}
 
 	public override void Apply() {
-		
+
 	}
 }
