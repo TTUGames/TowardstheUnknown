65ac6b378f215ab65b30608b58be1668abcba65b
raphael lesbros <raphael.lesbros@gmail.com>
2023-01-31 19:57:07 +0100

    Fixed line of sight

--- Assets/Scripts/Map/Tile/TileConstraints/LineOfSightConstraint.cs
+++ Assets/Scripts/Map/Tile/TileConstraints/LineOfSightConstraint.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class LineOfSightConstraint : TileConstraint {
 	public override bool isValid(Tile origin, Tile tile) {
 		RaycastHit hit;
-		Physics.Raycast(origin.transform.position + Vector3.up, tile.transform.position - origin.transform.position, out hit, (tile.transform.position - origin.transform.position).magnitude);
+		Vector3 raycastOrigin = new Vector3(origin.transform.position.x, origin.GetComponent<Collider>().bounds.max.y + 0.1f, origin.transform.position.z);
+		Physics.Raycast(raycastOrigin, tile.transform.position - origin.transform.position, out hit, (tile.transform.position - origin.transform.position).magnitude);
 		return hit.collider == null || (hit.collider.GetComponent<EntityStats>() != null && hit.collider.GetComponent<EntityStats>() == tile.GetEntity());
 	}
 }
