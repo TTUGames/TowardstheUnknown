73d6b6af280996ca4dffb3c334d0842ec1a49379
Laconiq <laconiqofficial@gmail.com>
2023-02-10 11:41:17 +0100

    KameikoSlashAttackPattern fix

--- Assets/Scripts/Player-NPC-Artifact/NPC/Kameiko/KameikoSlashAttackPattern.cs
+++ Assets/Scripts/Player-NPC-Artifact/NPC/Kameiko/KameikoSlashAttackPattern.cs
@@ -4,7 +4,7 @@ public class KameikoSlashAttackPattern : EnemyPattern {
 	public override void Init() {
 		patternDuration = 2f;
 		range = new LineAttackTS(1, 1);
-		vfxInfos.Add(new VFXInfo("VFX/00-Prefab/KameikoSlashAttackPattern", VFXInfo.Target.TARGETTILE, 0f));
+		vfxInfos.Add(new VFXInfo("VFX/00-Prefab/KameikoSlashAttackPattern", VFXInfo.Target.TARGETTILE, 0.7f));
 		targetType = EntityType.PLAYER;
 		animStateName = "KameikoSlashAttackPattern";
 	}
