c56025917bd7deee71cd88e9f377a77ece002567
Laconiq <laconiqofficial@gmail.com>
2023-02-11 16:03:42 +0100

    Update : Dissolving

--- Assets/Scripts/Player-NPC-Artifact/Player/Dissolving.cs
+++ Assets/Scripts/Player-NPC-Artifact/Player/Dissolving.cs
@@ -9,9 +9,15 @@ public enum WeaponEnum
 
 public class Dissolving : MonoBehaviour
 {
-    [SerializeField] private float      dissolveSpeed = 1f;
+    [SerializeField] private float dissolveSpeed = 1f;
     [SerializeField] private GameObject sword;
     [SerializeField] private GameObject gun;
+
+    public void Start()
+    {
+        DissolveGun();
+        UndissolveSword();
+    }
 
     public void DissolveAll()
     {
@@ -36,36 +42,46 @@ public class Dissolving : MonoBehaviour
     {
         List<Material> lm = new List<Material>();
         sword.GetComponent<MeshRenderer>().GetMaterials(lm);
-        StartCoroutine(Fade(lm[0], -2, dissolveSpeed));
+        StartCoroutine(Fade(lm[0], -2, dissolveSpeed, "sword", true));
     }
 
     public void UndissolveSword()
     {
+        sword.SetActive(true);
         List<Material> lm = new List<Material>();
         sword.GetComponent<MeshRenderer>().GetMaterials(lm);
-        StartCoroutine(Fade(lm[0], 5, dissolveSpeed));
+        StartCoroutine(Fade(lm[0], 5, dissolveSpeed, "sword", false));
     }
-    
+
     public void DissolveGun()
     {
         List<Material> lm = new List<Material>();
         gun.GetComponent<MeshRenderer>().GetMaterials(lm);
-        StartCoroutine(Fade(lm[0], -2, dissolveSpeed));
+        StartCoroutine(Fade(lm[0], -2, dissolveSpeed, "gun", true));
     }
 
     public void UndissolveGun()
     {
+        gun.SetActive(true);
         List<Material> lm = new List<Material>();
         gun.GetComponent<MeshRenderer>().GetMaterials(lm);
-        StartCoroutine(Fade(lm[0], 5, dissolveSpeed));
+        StartCoroutine(Fade(lm[0], 5, dissolveSpeed, "gun", false));
     }
 
-    IEnumerator Fade(Material material, float target, float time)
+    IEnumerator Fade(Material material, float target, float time, string weapon, bool dissolve)
     {
         while (material.GetFloat("_DissolvePosition") != target)
         {
-            material.SetFloat("_DissolvePosition", Mathf.MoveTowards(material.GetFloat("_DissolvePosition"), target, time)); // possibility to put over time if needed : Time.deltaTime / time
+            material.SetFloat("_DissolvePosition", Mathf.MoveTowards(material.GetFloat("_DissolvePosition"), target, time));
             yield return null;
+        }
+
+        if (dissolve)
+        {
+            if (weapon == "sword")
+                sword.SetActive(false);
+            else if (weapon == "gun")
+                gun.SetActive(false);
         }
     }
 }
