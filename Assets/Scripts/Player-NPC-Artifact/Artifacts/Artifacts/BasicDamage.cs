5813b0af8a3a610f6a8acbcdf63f769afce28d1e
Laconiq <laconiqofficial@gmail.com>
2023-02-12 18:02:00 +0100

    Fix BasicDamage inventory size

--- Assets/Scripts/Player-NPC-Artifact/Artifacts/Artifacts/BasicDamage.cs
+++ Assets/Scripts/Player-NPC-Artifact/Artifacts/Artifacts/BasicDamage.cs
@@ -21,7 +21,7 @@ public class BasicDamage : SingleTargetArtifact
 
         slots = new List<Vector2Int>()
         {
-            new Vector2Int(0,0),
+            new Vector2Int(1,1),
             new Vector2Int(1,0),
             new Vector2Int(0,1),
         };
