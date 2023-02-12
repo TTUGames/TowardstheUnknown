99941e6a602e13237fe27f8dec0cf2d246936a79
Laconiq <laconiqofficial@gmail.com>
2023-02-12 11:27:03 +0100

    Cataclysm VFX rescale + add animation

--- Assets/Scripts/Player-NPC-Artifact/NPC/Drareg/Patterns/DraregUltimateFail.cs
+++ Assets/Scripts/Player-NPC-Artifact/NPC/Drareg/Patterns/DraregUltimateFail.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class DraregUltimateFail : EnemyPattern {
 	public override void Init() {
-		patternDuration = 4f;
+		patternDuration = 7f;
 		range = new CircleTileSearch(0, int.MaxValue);
 		vfxInfos.Add(new VFXInfo("VFX/00-Prefab/Cataclysm", VFXInfo.Target.SOURCETILE, 0f));
 		targetType = EntityType.PLAYER;
+		animStateName = "Cataclysm";
 	}
 
 	public override void Use(EntityStats source, EntityStats target) {
