c77bcaeff702269a603fedddbf2c6e9b21562f85
Lucas <Inceept@gmail.com>
2023-02-18 15:37:51 +0100

    Retour vers le passé
    (comme dans code lyoko)

--- Assets/Scripts/Player-NPC-Artifact/Player/TetrisInventory/InventoryManager.cs
+++ Assets/Scripts/Player-NPC-Artifact/Player/TetrisInventory/InventoryManager.cs
@@ -17,11 +17,11 @@ public class InventoryManager : MonoBehaviour
         List<Artifact> startingArtifacts = new List<Artifact>()
         {
             //new EchoBomb(),
+            new ProtectiveEnvelope(),
             new Barrier(),
-            new CriticalShot(),
-            new Gunshot(),
-            new Bastion(),
-            new Haunting()
+            new BasicDamage(),
+            new SlashAttack(),
+            new ClearRoomArtifact()
         };
 
 
