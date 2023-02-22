4f149ce5f27b76a25d82d9e7fa0861402ebb19f2
Laconiq <laconiqofficial@gmail.com>
2023-02-22 14:08:36 +0100

    Add offset hover entity

--- Assets/InfoEntity.cs
+++ Assets/InfoEntity.cs
@@ -4,6 +4,10 @@ using System.Collections.Generic;
 
 public class InfoEntity : MonoBehaviour
 {
+    [SerializeField] private float upOffset = 15f;
+    [SerializeField] private float downOffset = -125f;
+    [SerializeField] private float leftOffset = 20f;
+
     private Camera cam;
     private GameObject infoEntityPrefab;
     private TMP_Text infoEntityTMP;
@@ -16,14 +20,14 @@ public class InfoEntity : MonoBehaviour
     {
         changeUI = GameObject.Find("UI").GetComponent<ChangeUI>();
         cam = GameObject.Find("Main Camera").GetComponent<Camera>();
-        
+
         List<TMP_Text> list = new List<TMP_Text>();
         list.AddRange(Resources.FindObjectsOfTypeAll<TMP_Text>());
 
         infoEntityTMP = list.Find(e => e.name == "InfoEntityTMP");
         nameEntityTMP = list.Find(e => e.name == "NameEntityTMP");
         infoEntityPrefab = infoEntityTMP.transform.parent.gameObject;
-        
+
         enemyStats = gameObject.GetComponent<EnemyStats>();
 
         entityName = Localization.GetEntityDescription(gameObject.name.Replace("(Clone)", "")).NAME;
@@ -34,13 +38,23 @@ public class InfoEntity : MonoBehaviour
         if (!changeUI.uIIsOpen && enemyStats.currentHealth > 0)
         {
             infoEntityPrefab.SetActive(true);
-            infoEntityPrefab.transform.position = cam.WorldToScreenPoint(transform.position);
+            Vector3 entityScreenPosition = cam.WorldToScreenPoint(transform.position);
+            if (entityScreenPosition.y > Screen.height / 2)
+            {
+                entityScreenPosition.y += upOffset;
+            }
+            else
+            {
+                entityScreenPosition.y -= downOffset;
+            }
+            entityScreenPosition.x -= leftOffset;
+            infoEntityPrefab.transform.position = entityScreenPosition;
             nameEntityTMP.text = entityName;
             infoEntityTMP.text = "<color=#e82a65>PV : " + enemyStats.currentHealth + " <color=#ffffff>|<color=#20D15F> PM : " + enemyStats.maxMovementPoints;
         }
         else
         {
-            infoEntityPrefab.SetActive(false);        
+            infoEntityPrefab.SetActive(false);
         }
     }
 
