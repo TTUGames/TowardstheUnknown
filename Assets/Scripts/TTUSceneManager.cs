fb2cf91638dc67a1c811aa831d349249938e2a9e
Laconiq <laconiqofficial@gmail.com>
2026-09-23 19:41:20 +0200

    fix: restore the pre-menu scene in the build
    Put 0-PreMenu, 1-Menu and 2-Game back as the only build scenes and
    restore SwitchFromPreMenuToMenu, which the "Lancer le jeu" button still
    calls. The serialized scene indices (death screen, restart) already
    assumed this order. Scene switching now relies on LoadSceneMode.Single
    instead of unloading the last loaded scene first, which Unity rejects.

--- Assets/Scripts/TTUSceneManager.cs
+++ Assets/Scripts/TTUSceneManager.cs
@@ -4,18 +4,24 @@ using UnityEngine.SceneManagement;
 
 public class TTUSceneManager : MonoBehaviour
 {
-    public static int menuIndex = 0;
-    public static int gameIndex = 1;
+    public static int preMenuIndex = 0;
+    public static int menuIndex = 1;
+    public static int gameIndex = 2;
 
     public static IEnumerator SwitchSceneCoroutine(int from, int to)
     {
-        yield return SceneManager.UnloadSceneAsync(from, UnloadSceneOptions.UnloadAllEmbeddedSceneObjects);
-        SceneManager.LoadScene(to, LoadSceneMode.Single);
+        // LoadSceneMode.Single already unloads the current scene; unloading it first is not allowed
+        yield return SceneManager.LoadSceneAsync(to, LoadSceneMode.Single);
     }
 
     public void SwitchScene(int from, int to)
     {
         StartCoroutine(SwitchSceneCoroutine(from, to));
+    }
+
+    public void SwitchFromPreMenuToMenu()
+    {
+        SwitchScene(preMenuIndex, menuIndex);
     }
 
     public void SwitchFromMenuToGame()
